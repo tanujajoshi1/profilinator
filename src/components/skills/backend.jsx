d283cee62b35b902c711ff349c459e5000300a48
ritz1804 <59580813+ritz1804@users.noreply.github.com>
2021-05-17 12:41:34 +0530

    Update backend.jsx

---
+++ src/components/skills/backend.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    boxShadow: 'none', 
+    width: '100%'
+  },
+}));
+
+function Item(props) {
+    return (
+        <img src = {props.img} />
+    );
+}
+
+function Backend() {
+    const classes = useStyles();
+    return (
+        <div className = "skills-sub">
+            <p>Backend Developement</p>
+
+            <div className={classes.root}>
+                <Grid container spacing={3}>
+                    <Grid item xs={3}>
+                    <label><Paper className={classes.paper}><input type="checkbox"/><Item img = "https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg" /></Paper></label>
+                    </Grid>
+                    <Grid item xs={3}>
+                    <label><Paper className={classes.paper}><input type="checkbox"/><Item img =  "https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg" /></Paper></label>
+                    </Grid>
+                    <Grid item xs={3}>
+                    <label><Paper className={classes.paper}><input type="checkbox"/><Item img =  "https://raw.githubusercontent.com/devicons/devicon/master/icons/go/go-original.svg" /></Paper></label>
+                    </Grid>
+                    <Grid item xs={3}>
+                    <label><Paper className={classes.paper}><input type="checkbox"/><Item img =  "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg" /></Paper></label>
+                    </Grid>
+                </Grid>
+            </div>
+
+            <div className={classes.root}>
+                <Grid container spacing={3}>
+                    <Grid item xs={3}>
+                    <label><Paper className={classes.paper}><input type="checkbox"/><Item img = "https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg" /></Paper></label>
+                    </Grid>
+                    <Grid item xs={3}>
+                    <label><Paper className={classes.paper}><input type="checkbox"/><Item img =  "https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg" /></Paper></label>
+                    </Grid>
+                    <Grid item xs={3}>
+                    <label><Paper className={classes.paper}><input type="checkbox"/><Item img =  "https://raw.githubusercontent.com/devicons/devicon/master/icons/go/go-original.svg" /></Paper></label>
+                    </Grid>
+                    <Grid item xs={3}>
+                    <label><Paper className={classes.paper}><input type="checkbox"/><Item img =  "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg" /></Paper></label>
+                    </Grid>
+                </Grid>
+            </div>
+
+            <div className={classes.root}>
+                <Grid container spacing={3}>
+                    <Grid item xs={3}>
+                    <label><Paper className={classes.paper}><input type="checkbox"/><Item img = "https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg" /></Paper></label>
+                    </Grid>
+                    <Grid item xs={3}>
+                    <label><Paper className={classes.paper}><input type="checkbox"/><Item img =  "https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg" /></Paper></label>
+                    </Grid>
+                    <Grid item xs={3}>
+                    <label><Paper className={classes.paper}><input type="checkbox"/><Item img =  "https://raw.githubusercontent.com/devicons/devicon/master/icons/go/go-original.svg" /></Paper></label>
+                    </Grid>
+                    <Grid item xs={3}>
+                    <label><Paper className={classes.paper}><input type="checkbox"/><Item img =  "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg" /></Paper></label>
+                    </Grid>
+                </Grid>
+            </div>
+            
+        </div>
+    );
+}
+
+export default Backend;
