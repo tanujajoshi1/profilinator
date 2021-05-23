bb9f63deea29e9df3f9ad31ddf7025f4a3f688e1
ritz1804 <59580813+ritz1804@users.noreply.github.com>
2021-05-23 19:11:48 +0530

    Update other.jsx

--- src/components/skills/other.jsx
+++ src/components/skills/other.jsx
@@ -53,13 +53,13 @@ function Other() {
             <div className={classes.root}>
                 <Grid container spacing={3}>
                     <Grid item xs={3}>
-                    <label><Paper className={classes.paper}><input type="checkbox" value={checked1} onChange={() => setChecked1(!checked1)} {...checked1 == true ? add("linux") : remove("linux")}/><Item img = "https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg" /></Paper></label>
+                    <label><Paper className={classes.paper}><input type="checkbox" value={checked1} onChange={() => setChecked1(!checked1)} {...checked1 == true ? add("https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg") : remove("https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg")}/><Item img = "https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg" /></Paper></label>
                     </Grid>
                     <Grid item xs={3}>
-                    <label><Paper className={classes.paper}><input type="checkbox" value={checked2} onChange={() => setChecked2(!checked2)} {...checked2 == true ? add("git") : remove("git")}/><Item img =  "https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" /></Paper></label>
+                    <label><Paper className={classes.paper}><input type="checkbox" value={checked2} onChange={() => setChecked2(!checked2)} {...checked2 == true ? add("https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg") : remove("https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg")}/><Item img =  "https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" /></Paper></label>
                     </Grid>
                     <Grid item xs={3}>
-                    <label><Paper className={classes.paper}><input type="checkbox" value={checked3} onChange={() => setChecked3(!checked3)} {...checked3 == true ? add("arduino") : remove("arduino")}/><Item img =  "https://cdn.worldvectorlogo.com/logos/arduino-1.svg" /></Paper></label>
+                    <label><Paper className={classes.paper}><input type="checkbox" value={checked3} onChange={() => setChecked3(!checked3)} {...checked3 == true ? add("https://cdn.worldvectorlogo.com/logos/arduino-1.svg") : remove("https://cdn.worldvectorlogo.com/logos/arduino-1.svg")}/><Item img =  "https://cdn.worldvectorlogo.com/logos/arduino-1.svg" /></Paper></label>
                     </Grid>
                 </Grid>
             </div>
