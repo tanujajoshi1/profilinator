fcd35ab0ee0024144bbca18020c3e3bcb12d971d
ritz1804 <59580813+ritz1804@users.noreply.github.com>
2021-05-23 19:07:03 +0530

    Update automation.jsx

--- src/components/skills/automation.jsx
+++ src/components/skills/automation.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import { AutomationList } from "../markdown";
+import { BaasList } from "../markdown";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -23,41 +23,54 @@ function Item(props) {
     );
 }
 
-let automationList = [];
+let baasList = [];
 
 function remove(element) {
-    const index = automationList.indexOf(element);
+    const index = baasList.indexOf(element);
     if (index > -1) {
-        automationList.splice(index, 1);
+        baasList.splice(index, 1);
     }
 }
 
 function add(element) {
-    automationList.push(element);
+    baasList.push(element);
 }
 
-function Automation() {
+function Baas() {
 
     const classes = useStyles();
     
     const [checked1, setChecked1] = React.useState(false);
-    automationList = [];
+    const [checked2, setChecked2] = React.useState(false);
+    const [checked3, setChecked3] = React.useState(false);
+    const [checked4, setChecked4] = React.useState(false);
+
+    baasList = [];
 
     return (
         <div className = "skills-sub">
-            <p>Automation</p>
+            <p>Baas</p>
 
             <div className={classes.root}>
                 <Grid container spacing={3}>
                     <Grid item xs={3}>
-                    <label><Paper className={classes.paper}><input type="checkbox" value={checked1} onChange={() => setChecked1(!checked1)} {...checked1 == true ? add("https://www.vectorlogo.zone/logos/zapier/zapier-icon.svg") : remove("https://www.vectorlogo.zone/logos/zapier/zapier-icon.svg")}/><Item img = "https://www.vectorlogo.zone/logos/zapier/zapier-icon.svg" /></Paper></label>
+                    <label><Paper className={classes.paper}><input type="checkbox" value={checked1} onChange={() => setChecked1(!checked1)} {...checked1 == true ? add("https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg") : remove("https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg")}/><Item img = "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg" /></Paper></label>
+                    </Grid>
+                    <Grid item xs={3}>
+                    <label><Paper className={classes.paper}><input type="checkbox" value={checked2} onChange={() => setChecked2(!checked2)} {...checked2 == true ? add("https://www.vectorlogo.zone/logos/appwriteio/appwriteio-icon.svg") : remove("https://www.vectorlogo.zone/logos/appwriteio/appwriteio-icon.svg")}/><Item img =  "https://www.vectorlogo.zone/logos/appwriteio/appwriteio-icon.svg" /></Paper></label>
+                    </Grid>
+                    <Grid item xs={3}>
+                    <label><Paper className={classes.paper}><input type="checkbox" value={checked3} onChange={() => setChecked3(!checked3)} {...checked3 == true ? add("https://docs.amplify.aws/assets/logo-dark.svg") : remove("https://docs.amplify.aws/assets/logo-dark.svg")}/><Item img =  "https://docs.amplify.aws/assets/logo-dark.svg" /></Paper></label>
+                    </Grid>
+                    <Grid item xs={3}>
+                    <label><Paper className={classes.paper}><input type="checkbox" value={checked4} onChange={() => setChecked4(!checked4)} {...checked4 == true ? add("https://www.vectorlogo.zone/logos/heroku/heroku-icon.svg") : remove("https://www.vectorlogo.zone/logos/heroku/heroku-icon.svg")}/><Item img =  "https://www.vectorlogo.zone/logos/heroku/heroku-icon.svg" /></Paper></label>
                     </Grid>
                 </Grid>
             </div>
-            {console.log(automationList)}
-            <AutomationList automationList = {automationList} />
+            {console.log(baasList)}
+            <BaasList baasList = {baasList} />
         </div>
     );
 }
 
-export default Automation;
+export default Baas;
