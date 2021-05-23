1fac4b6f3e74dce87ca559cb667f0beb88a727f6
ritz1804 <59580813+ritz1804@users.noreply.github.com>
2021-05-23 19:08:48 +0530

    Update database.jsx

--- src/components/skills/database.jsx
+++ src/components/skills/database.jsx
@@ -63,33 +63,16 @@ function Database() {
             <div className={classes.root}>
                 <Grid container spacing={3}>
                     <Grid item xs={3}>
-                    <label><Paper className={classes.paper}><input type="checkbox" value={checked1} onChange={() => setChecked1(!checked1)} {...checked1 == true ? add("mongodb") : remove("mongodb")}/><Item img = "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg" /></Paper></label>
+                    <label><Paper className={classes.paper}><input type="checkbox" value={checked1} onChange={() => setChecked1(!checked1)} {...checked1 == true ? add("https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg") : remove("https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg")}/><Item img = "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg" /></Paper></label>
                     </Grid>
                     <Grid item xs={3}>
-                    <label><Paper className={classes.paper}><input type="checkbox" value={checked2} onChange={() => setChecked2(!checked2)} {...checked2 == true ? add("mysql") : remove("mysql")}/><Item img =  "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" /></Paper></label>
+                    <label><Paper className={classes.paper}><input type="checkbox" value={checked2} onChange={() => setChecked2(!checked2)} {...checked2 == true ? add("https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg") : remove("https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg")}/><Item img =  "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" /></Paper></label>
                     </Grid>
                     <Grid item xs={3}>
-                    <label><Paper className={classes.paper}><input type="checkbox" value={checked3} onChange={() => setChecked3(!checked3)} {...checked3 == true ? add("postgresql") : remove("postgresql")}/><Item img =  "https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg" /></Paper></label>
+                    <label><Paper className={classes.paper}><input type="checkbox" value={checked3} onChange={() => setChecked3(!checked3)} {...checked3 == true ? add("https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg") : remove("https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg")}/><Item img =  "https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg" /></Paper></label>
                     </Grid>
                     <Grid item xs={3}>
-                    <label><Paper className={classes.paper}><input type="checkbox" value={checked4} onChange={() => setChecked4(!checked4)} {...checked4 == true ? add("redis") : remove("redis")}/><Item img =  "https://raw.githubusercontent.com/devicons/devicon/master/icons/redis/redis-original-wordmark.svg" /></Paper></label>
-                    </Grid>
-                </Grid>
-            </div>
-
-            <div className={classes.root}>
-                <Grid container spacing={3}>
-                    <Grid item xs={3}>
-                    <label><Paper className={classes.paper}><input type="checkbox" value={checked5} onChange={() => setChecked5(!checked5)} {...checked5 == true ? add("oracle") : remove("oracle")}/><Item img = "https://raw.githubusercontent.com/devicons/devicon/master/icons/oracle/oracle-original.svg" /></Paper></label>
-                    </Grid>
-                    <Grid item xs={3}>
-                    <label><Paper className={classes.paper}><input type="checkbox" value={checked6} onChange={() => setChecked6(!checked6)} {...checked6 == true ? add("cassandra") : remove("cassandra")}/><Item img =  "https://www.vectorlogo.zone/logos/apache_cassandra/apache_cassandra-icon.svg" /></Paper></label>
-                    </Grid>
-                    <Grid item xs={3}>
-                    <label><Paper className={classes.paper}><input type="checkbox" value={checked7} onChange={() => setChecked7(!checked7)} {...checked7 == true ? add("couchdb") : remove("couchdb")}/><Item img =  "https://raw.githubusercontent.com/devicons/devicon/0d6c64dbbf311879f7d563bfc3ccf559f9ed111c/icons/couchdb/couchdb-original.svg" /></Paper></label>
-                    </Grid>
-                    <Grid item xs={3}>
-                    <label><Paper className={classes.paper}><input type="checkbox" value={checked8} onChange={() => setChecked8(!checked8)} {...checked8 == true ? add("hive") : remove("hive")}/><Item img =  "https://www.vectorlogo.zone/logos/apache_hive/apache_hive-icon.svg" /></Paper></label>
+                    <label><Paper className={classes.paper}><input type="checkbox" value={checked4} onChange={() => setChecked4(!checked4)} {...checked4 == true ? add("https://raw.githubusercontent.com/devicons/devicon/master/icons/redis/redis-original-wordmark.svg") : remove("https://raw.githubusercontent.com/devicons/devicon/master/icons/redis/redis-original-wordmark.svg")}/><Item img =  "https://raw.githubusercontent.com/devicons/devicon/master/icons/redis/redis-original-wordmark.svg" /></Paper></label>
                     </Grid>
                 </Grid>
             </div>
@@ -97,16 +80,16 @@ function Database() {
             <div className={classes.root}>
                 <Grid container spacing={3}>
                     <Grid item xs={3}>
-                    <label><Paper className={classes.paper}><input type="checkbox" value={checked9} onChange={() => setChecked9(!checked9)} {...checked9 == true ? add("realm") : remove("realm")}/><Item img = "https://raw.githubusercontent.com/bestofjs/bestofjs-webui/8665e8c267a0215f3159df28b33c365198101df5/public/logos/realm.svg" /></Paper></label>
+                    <label><Paper className={classes.paper}><input type="checkbox" value={checked5} onChange={() => setChecked5(!checked5)} {...checked5 == true ? add("https://raw.githubusercontent.com/devicons/devicon/master/icons/oracle/oracle-original.svg") : remove("https://raw.githubusercontent.com/devicons/devicon/master/icons/oracle/oracle-original.svg")}/><Item img = "https://raw.githubusercontent.com/devicons/devicon/master/icons/oracle/oracle-original.svg" /></Paper></label>
                     </Grid>
                     <Grid item xs={3}>
-                    <label><Paper className={classes.paper}><input type="checkbox" value={checked10} onChange={() => setChecked10(!checked10)} {...checked10 == true ? add("mariadb") : remove("mariadb")}/><Item img =  "https://www.vectorlogo.zone/logos/mariadb/mariadb-icon.svg" /></Paper></label>
+                    <label><Paper className={classes.paper}><input type="checkbox" value={checked6} onChange={() => setChecked6(!checked6)} {...checked6 == true ? add("https://www.vectorlogo.zone/logos/apache_cassandra/apache_cassandra-icon.svg") : remove("https://www.vectorlogo.zone/logos/apache_cassandra/apache_cassandra-icon.svg")}/><Item img =  "https://www.vectorlogo.zone/logos/apache_cassandra/apache_cassandra-icon.svg" /></Paper></label>
                     </Grid>
                     <Grid item xs={3}>
-                    <label><Paper className={classes.paper}><input type="checkbox" value={checked11} onChange={() => setChecked11(!checked11)} {...checked11 == true ? add("cockroachdb") : remove("cockroachdb")}/><Item img =  "https://cdn.worldvectorlogo.com/logos/cockroachdb.svg" /></Paper></label>
+                    <label><Paper className={classes.paper}><input type="checkbox" value={checked7} onChange={() => setChecked7(!checked7)} {...checked7 == true ? add("https://raw.githubusercontent.com/devicons/devicon/0d6c64dbbf311879f7d563bfc3ccf559f9ed111c/icons/couchdb/couchdb-original.svg") : remove("https://raw.githubusercontent.com/devicons/devicon/0d6c64dbbf311879f7d563bfc3ccf559f9ed111c/icons/couchdb/couchdb-original.svg")}/><Item img =  "https://raw.githubusercontent.com/devicons/devicon/0d6c64dbbf311879f7d563bfc3ccf559f9ed111c/icons/couchdb/couchdb-original.svg" /></Paper></label>
                     </Grid>
                     <Grid item xs={3}>
-                    <label><Paper className={classes.paper}><input type="checkbox" value={checked12} onChange={() => setChecked12(!checked12)} {...checked12 == true ? add("elasticicon") : remove("elasticicon")}/><Item img =  "https://www.vectorlogo.zone/logos/elastic/elastic-icon.svg" /></Paper></label>
+                    <label><Paper className={classes.paper}><input type="checkbox" value={checked8} onChange={() => setChecked8(!checked8)} {...checked8 == true ? add("https://www.vectorlogo.zone/logos/apache_hive/apache_hive-icon.svg") : remove("https://www.vectorlogo.zone/logos/apache_hive/apache_hive-icon.svg")}/><Item img =  "https://www.vectorlogo.zone/logos/apache_hive/apache_hive-icon.svg" /></Paper></label>
                     </Grid>
                 </Grid>
             </div>
@@ -114,7 +97,24 @@ function Database() {
             <div className={classes.root}>
                 <Grid container spacing={3}>
                     <Grid item xs={3}>
-                    <label><Paper className={classes.paper}><input type="checkbox" value={checked13} onChange={() => setChecked13(!checked13)} {...checked13 == true ? add("sqlite") : remove("sqlite")}/><Item img = "https://www.vectorlogo.zone/logos/sqlite/sqlite-icon.svg" /></Paper></label>
+                    <label><Paper className={classes.paper}><input type="checkbox" value={checked9} onChange={() => setChecked9(!checked9)} {...checked9 == true ? add("https://raw.githubusercontent.com/bestofjs/bestofjs-webui/8665e8c267a0215f3159df28b33c365198101df5/public/logos/realm.svg") : remove("https://raw.githubusercontent.com/bestofjs/bestofjs-webui/8665e8c267a0215f3159df28b33c365198101df5/public/logos/realm.svg")}/><Item img = "https://raw.githubusercontent.com/bestofjs/bestofjs-webui/8665e8c267a0215f3159df28b33c365198101df5/public/logos/realm.svg" /></Paper></label>
+                    </Grid>
+                    <Grid item xs={3}>
+                    <label><Paper className={classes.paper}><input type="checkbox" value={checked10} onChange={() => setChecked10(!checked10)} {...checked10 == true ? add("https://www.vectorlogo.zone/logos/mariadb/mariadb-icon.svg") : remove("https://www.vectorlogo.zone/logos/mariadb/mariadb-icon.svg")}/><Item img =  "https://www.vectorlogo.zone/logos/mariadb/mariadb-icon.svg" /></Paper></label>
+                    </Grid>
+                    <Grid item xs={3}>
+                    <label><Paper className={classes.paper}><input type="checkbox" value={checked11} onChange={() => setChecked11(!checked11)} {...checked11 == true ? add("https://cdn.worldvectorlogo.com/logos/cockroachdb.svg") : remove("https://cdn.worldvectorlogo.com/logos/cockroachdb.svg")}/><Item img =  "https://cdn.worldvectorlogo.com/logos/cockroachdb.svg" /></Paper></label>
+                    </Grid>
+                    <Grid item xs={3}>
+                    <label><Paper className={classes.paper}><input type="checkbox" value={checked12} onChange={() => setChecked12(!checked12)} {...checked12 == true ? add("https://www.vectorlogo.zone/logos/elastic/elastic-icon.svg") : remove("https://www.vectorlogo.zone/logos/elastic/elastic-icon.svg")}/><Item img =  "https://www.vectorlogo.zone/logos/elastic/elastic-icon.svg" /></Paper></label>
+                    </Grid>
+                </Grid>
+            </div>
+
+            <div className={classes.root}>
+                <Grid container spacing={3}>
+                    <Grid item xs={3}>
+                    <label><Paper className={classes.paper}><input type="checkbox" value={checked13} onChange={() => setChecked13(!checked13)} {...checked13 == true ? add("https://www.vectorlogo.zone/logos/sqlite/sqlite-icon.svg") : remove("https://www.vectorlogo.zone/logos/sqlite/sqlite-icon.svg")}/><Item img = "https://www.vectorlogo.zone/logos/sqlite/sqlite-icon.svg" /></Paper></label>
                     </Grid>
                 </Grid>
             </div>
